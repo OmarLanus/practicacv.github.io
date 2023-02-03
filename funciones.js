c727344ca41ca9fad4f7ec36fe8103a0fc8df023
OmarLanus <124325988+OmarLanus@users.noreply.github.com>
2023-02-03 17:28:00 -0300

    initial commit

---
+++ funciones.js
@@ -0,0 +1,8 @@
+function cambiar(){
+    var header = document.querySelector("header");
+    header.style.display = "none";
+}
+function mostrar(){
+    var header = document.querySelector("header");
+    header.style.display = "";
+}
